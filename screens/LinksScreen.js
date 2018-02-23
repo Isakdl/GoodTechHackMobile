c4660b0e87278740843a0e95c3a8f49bf349f502
Isak Dunér <isak.duner@gmail.com>
2018-02-23 17:55:01 +0100

    my location is now drawn on the map

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Platform } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import { MapView } from 'expo';
 import {Marker} from 'react-native-maps'
+import { Constants, Location, Permissions } from 'expo';
 
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -10,6 +11,35 @@ import gql from 'graphql-tag';
 class LinksScreen extends React.Component {
     static navigationOptions = {
       header: null,
+    };
+
+    state = {
+      location: null,
+      errorMessage: null,
+    };
+
+    componentWillMount() {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        this.setState({
+          errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+        });
+      } else {
+        this._getLocationAsync();
+      }
+    }
+
+    _getLocationAsync = async () => {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== 'granted') {
+        this.setState({
+          errorMessage: 'Permission to access location was denied',
+        });
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+      this.setState({ location });
     };
 
     placeMarker = (markers) => {
@@ -25,20 +55,25 @@ class LinksScreen extends React.Component {
     }
 
     render() {
-        return (
-            <MapView
-              style={{ flex: 1 }}
-              initialRegion={{
-                  latitude: 37.78825,
-                  longitude: -122.4324,
-                  latitudeDelta: 0.0922,
-                  longitudeDelta: 0.0421,
-              }}
-            >
-              {this.placeMarker([{latitude: 37.78825,
-              longitude: -122.4324,id:1}])}
-            </MapView>
+      let location = this.state.location
 
+      if(!location){
+        return <View></View>
+      }
+
+      return (
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+          >
+            {this.placeMarker([{latitude: location.coords.latitude,
+            longitude: location.coords.longitude,id:1}])}
+          </MapView>
     )
 
   }
